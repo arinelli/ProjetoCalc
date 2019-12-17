96e4bde7f5d156c9fe432e51960fb1cd9c5ddcfa
Andre Arinelli <andre.arinelli11@gmail.com>
2019-12-16 21:33:40 -0300

    Signed-off-by: Andre Arinelli <andre.arinelli11@gmail.com>

--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,4 @@
+
 <?php
 
 /*
@@ -12,5 +13,32 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
+Route::get('/users', 'ControladorUser@index');
+Route::get('/users/novo', 'ControladorUser@create');
+Route::post('/users', 'ControladorUser@store');
+Route::get('/users/apagar/{id}', 'ControladorUser@destroy');
+Route::get('/users/editar/{id}', 'ControladorUser@edit');
+Route::post('/users/{id}', 'ControladorUser@update');
+
+
+Route::get('/enderecos', 'ControladorEndereco@index');
+Route::get('/enderecos/novo', 'ControladorEnderecos@create');
+Route::post('/enderecos', 'ControladorEndereco@store');
+Route::get('/enderecos/apagar/{id}', 'ControladorEndereco@destroy');
+Route::get('/enderecos/editar/{id}', 'ControladorEndereco@edit');
+Route::post('/enderecos/{id}', 'ControladorEndereco@update');
+
+
+
+Route::get('/alunos', 'ControladorAluno@index');
+Route::get('/alunos/novo', 'ControladorAluno@create');
+Route::post('/alunos', 'ControladorAluno@store');
+Route::get('/alunos/apagar/{id}', 'ControladorAluno@destroy');
+Route::get('/alunos/editar/{id}', 'ControladorAluno@edit');
+Route::post('/alunos/{id}', 'ControladorAluno@update');
+
+
+
